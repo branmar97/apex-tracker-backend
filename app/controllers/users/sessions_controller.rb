07e84522d689dea2afcba0063c826720dbcf3405
branmar97 <brandonmar97@gmail.com>
2021-03-01 14:05:29 -0500

    Setup user serializer

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,27 +2,33 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
+  
+  private
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        message: 'Logged in successfully.'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        message: "Invalid Email or Password"
+      }, status: :unauthorized
+    end
+  end
+  
+  def respond_to_on_destroy
+    if user_signed_in? 
+      render json: {
+        status: 200,
+        message: "Logged out successfully"
+      }, status: :ok
+    else
+      render json: { 
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
 end
